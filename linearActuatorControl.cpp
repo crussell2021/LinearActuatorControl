e9d5a574d0b4ffd6693f50309a3ee3b17722b2b0
crussell2021 <50681298+crussell2021@users.noreply.github.com>
2019-11-19 14:46:38 -0800

    implemented functions for user defined control

--- linearActuatorControl.cpp
+++ linearActuatorControl.cpp
@@ -1,5 +1,50 @@
 #include "linearActuatorControl.h"
 
-LinearActuatorControl::LinearActuatorControl() {
+LinearActuatorControl::LinearActuatorControl(PinName controlModeSwitch = p14, PinName runModeSwitch = p15, PinName act1pwm = p21, PinName act2pwm = p22, PinName act3pwm = p23, PinName act4pwm = p24, PinName act1btn = p17, PinName act2btn = p18, PinName act3btn = p19, PinName act4btn = p20) : _controlMode(controlModeSwitch), _runMode(runModeSwitch), _linAct1(act1pwm), _linAct2(act2pwm), _linAct3(act3pwm), _linAct4(act4pwm), _actBtn1(act1btn), _actBtn2(act2btn), _actBtn3(act3btn), _actBtn4(act4btn) {
+    for (int i = 0; i < 4; i++) {
+        _idlePositions[i] = 0;
+        _actPositions[i] = 0;
+    }
 
+    _linAct1.period_ms(20);
+    _linAct2.period_ms(20);
+    _linAct3.period_ms(20);
+    _linAct4.period_ms(20);
+
+}
+
+void LinearActuatorControl::_refreshActuators() {
+    if (_runMode) {
+        _linAct1.pulsewidth_us(1000*(1+_actPositions[0]));
+        _linAct2.pulsewidth_us(1000*(1+_actPositions[1]));
+        _linAct3.pulsewidth_us(1000*(1+_actPositions[2]));
+        _linAct4.pulsewidth_us(1000*(1+_actPositions[3]));
+    } else {
+        _linAct1.pulsewidth_us(1000*(1+_idlePositions[0]));
+        _linAct2.pulsewidth_us(1000*(1+_idlePositions[1]));
+        _linAct3.pulsewidth_us(1000*(1+_idlePositions[2]));
+        _linAct4.pulsewidth_us(1000*(1+_idlePositions[3]));
+    }
+}
+
+void LinearActuatorControl::_setActuatorPositions(int actuator, float position) {
+    _actPositions[actuator] = position;
+}
+
+void LinearActuatorControl::_setActuatorIdlePositions(int actuator, float position) {
+    _idlePositions[actuator] = position;
+}
+
+void LinearActuatorControl::setIdlePositions(float pos1, float pos2, float pos3, float pos4) {
+    _idlePositions[0] = pos1;
+    _idlePositions[1] = pos2;
+    _idlePositions[2] = pos3;
+    _idlePositions[3] = pos4;
+}
+
+bool LinearActuatorControl::userSequence() {
+    if (_controlMode && _runMode) {
+        return true;
+    } 
+    return false;
 }
--- linearActuatorControl.h
+++ linearActuatorControl.h
@@ -5,10 +5,36 @@
 
 class LinearActuatorControl {
     private:
+        PwmOut _linAct1;
+        PwmOut _linAct2;
+        PwmOut _linAct3;
+        PwmOut _linAct4;
+        DigitalIn _controlMode;
+        DigitalIn _runMode;
 
+        InterruptIn _actBtn1;
+        InterruptIn _actBtn2;
+        InterruptIn _actBtn3;
+        InterruptIn _actBtn4;
 
+        float _idlePositions[4];    //stores values of idle positions, deafult all 0, user defined with setIdlePositions
+        float _actPositions[4];           //stores current positions of the actuators, updated by _setActuatorPos
+
+        void _setActuatorPositions(int actuator, float position);     //updates current actuator positions
+        void _setActuatorIdlePositions(int actuator, float position); //changes the idle positions
+        void _refreshActuators();                                     //sets the pulsewidth of each pwm based on _actPositions
     public:
+        //contructor, pins setup for LPC1768 by default so no parameters are needed
+        LinearActuatorControl(PinName controlModeSwitch, PinName runModeSwitch, PinName act1pwm, PinName act2pwm, PinName act3pwm, PinName act4pwm, PinName act1btn, PinName act2btn, PinName act3btn, PinName act4btn);
 
-}
+        //user function to command actuators to move, takes any float value from 0 to 1 for the percent extension of the actuator (0.45 = 45% extended)
+        void move(float pos1, float pos2, float pos3, float pos4);
+
+        //similar to move, but sets the idle positions for the actuators
+        void setIdlePositions(float pos1, float pos2, float pos3, float pos4);
+
+        //used to check the state of the machine, will return true if in auto+run mode
+        bool userSequence();
+};
 
 #endif
